a41ca15518d0b3595a18b6b5c70c12eb2832438a
Nate <nate@2bttns.com>
2022-07-03 22:38:49 -0400

    Cart height fix

--- pages/cart/index.jsx
+++ pages/cart/index.jsx
@@ -24,7 +24,7 @@ const CartPage = () => {
 		router.push("/checkout")
 	}
 	return (
-		<div className="flex flex-col h-screen">
+		<div className="flex flex-col min-h-screen">
 			<Navbar/>
 			<>{
 				cartItems.length > 0 ?

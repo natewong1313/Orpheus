16b48e6a68426ed16f3db8933c708b721cdda137
Nate <nate@2bttns.com>
2022-07-05 17:42:02 -0400

    Fix singleton issue

--- utils/stripe/loadStripePrivate.ts
+++ utils/stripe/loadStripePrivate.ts
@@ -1,9 +1,8 @@
 import Stripe from "stripe"
 
-let stripe: Stripe | null
 export default function loadStripePrivate() {
-	if (!stripe) {
-		stripe = new Stripe(process.env.STRIPE_SECRET_KEY, { apiVersion: "2020-08-27" })
+	if (!global.stripe) {
+		global.stripe = new Stripe(process.env.STRIPE_SECRET_KEY, { apiVersion: "2020-08-27" })
 	}
-	return stripe
+	return global.stripe
 }

b425ff92a25c52d0d89a75ff4b42492ddf6270ce
Nate <nate@2bttns.com>
2022-07-06 21:03:46 -0400

    Add stripe badge to checkout navbar

--- components/pages/checkout/Navbar.tsx
+++ components/pages/checkout/Navbar.tsx
@@ -1,13 +1,15 @@
 import React from "react"
 import Link from "next/link"
 import { HiArrowLeft } from "react-icons/hi"
+import Image from "next/image"
+import StripeBadge from "@/public/photos/stripe-badge.svg"
 
 const Navbar = () => {
 	return (
 		<div className="px-6 py-4 flex items-center border-b border-b-gray-200 justify-between bg-white">
 			{/* Return button */}
 			<Link href="/cart">
-				<a className="text-sm text-gray-500 hover:text-black font-medium flex flex-1 items-center">
+				<a className="text-sm text-gray-500 hover:text-black font-medium flex items-center">
 					<HiArrowLeft className="mr-2"/>
 					<span className="hidden md:block">Back to shopping cart</span>
 					<span className="block md:hidden">Back</span>
@@ -16,7 +18,12 @@ const Navbar = () => {
 			<div>
 				<h1 className="font-bold text-xl">Checkout</h1>
 			</div>
-			<div className="flex-1"/>
+			<div className="text-right items-center flex">
+				{/* Stripe badge element */}
+				<a href="https://stripe.com/" target="_blank" className="flex items-center">
+					<Image src={StripeBadge} width={152} height={42} loading="eager" priority={true} draggable={false}/>
+				</a>
+			</div>
 		</div>
 	)
 }

def35a2455120520c1b77ebaabce3599d4045ec0
Nate <nate@2bttns.com>
2022-07-07 22:33:57 -0400

    Checkout navbar mobile tweaks

--- components/pages/checkout/Navbar.tsx
+++ components/pages/checkout/Navbar.tsx
@@ -9,19 +9,24 @@ const Navbar = () => {
 		<div className="px-6 py-4 flex items-center border-b border-b-gray-200 justify-between bg-white">
 			{/* Return button */}
 			<Link href="/cart">
-				<a className="text-sm text-gray-500 hover:text-black font-medium flex items-center">
-					<HiArrowLeft className="mr-2"/>
+				<a className="text-sm text-gray-500 hover:text-black font-medium flex-1 flex flex-row items-center">
+					<HiArrowLeft className="mr-2 text-lg"/>
 					<span className="hidden md:block">Back to shopping cart</span>
 					<span className="block md:hidden">Back</span>
 				</a>
 			</Link>
 			<div>
-				<h1 className="font-bold text-xl">Checkout</h1>
+				<h1 className="font-bold text-lg sm:text-xl">Checkout</h1>
 			</div>
-			<div className="text-right items-center flex">
+			<div className="text-right items-center flex-1">
 				{/* Stripe badge element */}
-				<a href="https://stripe.com/" target="_blank" className="flex items-center">
-					<Image src={StripeBadge} width={152} height={42} loading="eager" priority={true} draggable={false}/>
+				<a href="https://stripe.com/" target="_blank" className="flex items-center h-8 w-24 sm:w-32 ml-auto">
+					<Image
+						src={StripeBadge}
+						loading="eager"
+						priority={true}
+						draggable={false}
+					/>
 				</a>
 			</div>
 		</div>

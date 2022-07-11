5cadec86b0bd252b832bdf315702178a4cce8db6
Nate <nate@2bttns.com>
2022-07-11 00:20:17 -0400

    Improve error handling

--- pages/api/products/delete.ts
+++ pages/api/products/delete.ts
@@ -7,17 +7,17 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
 		return res.status(405).json({ success: false, message: "Request method not allowed" })
 	}
 
-	// try {
-	await prisma.product.delete({
-		where: { id: req.body.id }
-	})
-	return res.status(200).json({ success: true })
-	// } catch (e) {
-	// 	if (!(e instanceof Prisma.PrismaClientKnownRequestError)) {
-	// 		throw(e)
-	// 	} else {
-	// 		return res.status(400).json({ success: false, message: "Product not found" })
-	// 	}
-	// }
+	try {
+		await prisma.product.delete({
+			where: { id: req.body.id }
+		})
+		return res.status(200).json({ success: true })
+	} catch (e) {
+		if (e.code === "P2025") {
+			return res.status(400).json({ success: false, message: "Product not found" })
+		} else {
+			throw(e)
+		}
+	}
 
 }

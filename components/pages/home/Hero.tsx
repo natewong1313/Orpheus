548df07564fe053af8016495a6c179dba09ccaf1
Nate <nate@2bttns.com>
2022-07-25 22:24:40 -0400

    Hero visual updates

--- components/pages/home/Hero.tsx
+++ components/pages/home/Hero.tsx
@@ -1,5 +1,6 @@
 import React from "react"
 import Image from "next/image"
+import Link from "next/link"
 import HeroImg from "@/public/pexels-tembela-bohle-1884581.jpg"
 
 const Hero = () => {
@@ -20,12 +21,13 @@ const Hero = () => {
 					<div className="h-full flex flex-col items-center justify-center space-y-6 sm:space-y-3">
 						{/* Content */}
 						<h1 className="text-white font-bold text-6xl">Welcome to Orpheus</h1>
-						<h2 className="text-gray-200 font-regular">An open-source ecommerce website builder. Customizable and
+						<h2 className="text-gray-200 font-regular">An open-source ecommerce website template. Customizable and
 							fully functional</h2>
-						<button className="bg-white rounded-md text-black font-semibold py-3 px-6 hover:bg-gray-200">
-							View Shop
-							{/* <HiArrowSmRight size={20} className="ml-0.5"/> */}
-						</button>
+						<Link href="/shop">
+							<a className="bg-white rounded-md text-black font-medium py-3 px-6 hover:bg-gray-200">
+								View Shop
+							</a>
+						</Link>
 					</div>
 				</div>
 			</div>
